5b0115d13109b4736f7d38b017d6da44000f2027
Steven Bartkiewicz <sbartkie@uncc.edu>
2020-11-30 14:16:51 -0500

    Lesson 2 changes

---
+++ routes/authRoutes.js
@@ -0,0 +1,29 @@
+// Import express to be able to use the Router 
+// Create routes for the authentication section of the web app
+
+const { Router } = require('express');
+
+const router = Router();
+
+// import exported methods from authController
+const authController = require('../controllers/authController')
+
+// Create post and get requests for signup and login
+
+router.get('/signup', authController.signup_get);
+
+router.post('/signup', authController.signup_post);
+
+router.get('/login', authController.login_get);
+
+router.post('/login', authController.login_post);
+
+module.exports = router;
+
+/*
+    Do the get or post methods change the 
+    router object such as the routes are created
+    within the object? When the router is exported, 
+    how are the methods exported and the corresponding 
+    callback functions exported along with the router object?
+*/
