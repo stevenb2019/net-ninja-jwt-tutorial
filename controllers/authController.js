3614778e0e234f7d899fa5818f3d6542d57c8fa5
Steven Bartkiewicz <sbartkie@uncc.edu>
2020-11-30 21:16:15 -0500

    Lesson 4 changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,11 +8,21 @@ module.exports.login_get = (req, res) => {
     res.render('signup');
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send('user signing up');
+// Make sure to say the function is asynchronous
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    }
+    catch{
+        console.log(err);
+        res.status(400).send('Error, user not created');
+    }
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     // This is called destructuring
     const { email, password } = req.body;
 
