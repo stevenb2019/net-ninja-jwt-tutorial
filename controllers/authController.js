cd90cef9d5d4bbca0a823e9be5d4a193041fc002
Steven Bartkiewicz <sbartkie@uncc.edu>
2020-12-03 03:45:20 -0500

    Final changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,15 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: ''};
 
+    // incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = 'that email is not registered';
+    }
+
+    if(err.message === 'incorrect password'){
+        errors.password = 'that password is incorrect';
+    }
+
     // duplicate error code
     if(err.code === 11000){
         errors.email = 'That email is already registered';
@@ -66,7 +75,25 @@ module.exports.login_post = async (req, res) => {
     // This is called destructuring
     const { email, password } = req.body;
 
-    res.send(email + " " + password);
+    try{
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({user: user._id});
+        
+    }
+    catch(err)
+    {
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
+    }
+
+    //res.send(email + " " + password);
     //res.send('user logging in');
+}
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 }
 
--- models/User.js
+++ models/User.js
@@ -70,6 +70,19 @@ const bcrypt = require('bcrypt');
     next();
  })
 
+ // static method to login user
+ userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({email});
+    if(user){
+      const auth = await bcrypt.compare(password, user.password);
+      if(auth){
+         return user;
+      }
+      throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+ }
+
 
  const User = mongoose.model('user', userSchema);
 
